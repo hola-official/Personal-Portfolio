51ee8b1e4fb262d5574ee97a205b8746350f35f6
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-09-17 22:00:36 +0100

    git update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "UI UX Designer", "Freelancer"],
+    strings: ["Frontend Developer", "UI UX Designer", "Google knowledge panel specialist", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
