83e7eb9ab563bc87042b1e731e5537cc10dfd2ae
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-10-12 02:35:36 +0100

    git update

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll 
+            sec.classList.add('show-animate');    
+        }
+        // if want to use animation that repeats on scroll 
+        else {
+            sec.classList.remove('show-animate');
         }
     })
     // sticky header
